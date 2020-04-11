bc2b76273aed2f74aad7a008a4f5dc387679d8e6
Nekmo <contacto@nekmo.com>
2020-04-12 00:37:30 +0200

    Removed STATICFILES_DIRS.

--- {{ cookiecutter.github_project_name }}/{{ cookiecutter.project_slug }}/settings/defaults.py
+++ {{ cookiecutter.github_project_name }}/{{ cookiecutter.project_slug }}/settings/defaults.py
@@ -85,7 +85,6 @@ STATIC_ROOT = None
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
 ]
 
 # MEDIA
