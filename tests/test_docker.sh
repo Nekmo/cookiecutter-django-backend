b408bb0516d8e2d21a46f2089de2b212f6c21f09
Nekmo <contacto@nekmo.com>
2020-05-11 03:18:27 +0200

    Issue #2: Test docker using travis

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -8,5 +8,6 @@ cookiecutter ../../ --no-input --overwrite-if-exists $@
 cd my-project-name
 sudo rm -rf data
 
-docker-compose build
-docker-compose up -d
+docker-compose -f docker-compose.develop.yml up -d --build
+
+curl http://127.0.0.1:8000 > /dev/null
